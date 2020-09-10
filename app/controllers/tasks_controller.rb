1c8e3b121fbc5fbf8e1eba504d1d749736ad1e32
yuki.takihara <takiharatakihara@gmail.com>
2020-09-10 09:08:31 +0000

    kadai lesson15 part1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,12 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
   
   def index
-    @tasks = Task.order(id: :desc).page(params[:page]).per(3)
+    if logged_in?
+      @task = current_user.tasks.build
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end
   end
   
   def show
@@ -13,11 +17,11 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = "Taskが正常に追加されました"
-      redirect_to @task
+      redirect_to root_url
     else
       flash.now[:danger] = "Taskが追加されませんでした"
       render :new
@@ -30,7 +34,7 @@ class TasksController < ApplicationController
   def update
     if @task.update(task_params)
       flash[:success] = "Taskは正常に更新されました"
-      redirect_to @task
+      redirect_to root_url
     else
       flash.now[:danger] = "Taskは更新されませんでした"
       render :edit
@@ -39,9 +43,8 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy
-    
     flash[:success] = "Taskは正常に削除されました"
-    redirect_to tasks_url
+    redirect_back(fallback_location: root_path)
   end
   
   private
@@ -52,6 +55,13 @@ class TasksController < ApplicationController
   
     def task_params
       params.require(:task).permit(:content, :status)
+    end
+    
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      unless @micropost
+        redirect_to root_url
+      end
     end
 
 end
